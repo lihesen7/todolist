38d0d3c8a75734d4855b3b4a50f42c7f078d6f0b
lihesen1999 <3502199077@qq.com>
2020-02-26 19:48:02 +0800

    react

---
+++ precache-manifest.a7256682e01345639a9398124305786c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5504eab52b05f36fd1c1c398e0cb6002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa7c0bb4865e99c3b2f",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "079d30027bfc12d23105",
+    "url": "/static/css/main.fc0ed9d5.chunk.css"
+  },
+  {
+    "revision": "2aa7c0bb4865e99c3b2f",
+    "url": "/static/js/2.85eeac32.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.85eeac32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079d30027bfc12d23105",
+    "url": "/static/js/main.8bd2670e.chunk.js"
+  },
+  {
+    "revision": "5aa38a02b1eff987103c",
+    "url": "/static/js/runtime-main.461f47ef.js"
+  }
+]);
